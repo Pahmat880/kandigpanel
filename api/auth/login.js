85018b08527dd75735a0abeadf3d4ef284ef2103
Pahmat880 <amay58909@gmail.com>
2025-08-18 11:17:49 +0700

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -32,7 +32,7 @@ export default async function handler(req, res) {
     }
 
     const payload = {
-      id: user.accountId, // Token sekarang berisi accountId
+      id: user.accountId,
       username: user.username,
       role: user.role,
       accountType: user.accountType
